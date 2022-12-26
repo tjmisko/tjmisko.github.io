9f6746aa7893f136824640fdbf5bae8d35516d7c
tjmisko <tjmisko@users.noreply.github.com>
2022-12-26 09:40:39 -0500

    flexbox for project cards created

--- projectScroll.js
+++ projectScroll.js
@@ -5,40 +5,78 @@ let carouselControlsUnclicked = true;
 
 // Initialize selected elements and width functions
 const projectTiles = document.querySelectorAll(".project-card");
-let carouselBox = document.querySelector(".carousel-box")
+let carouselBox = document.querySelector(".carousel-box");
 
+// Assuming a spacing between tiles of 10px and uniform tile width
+function nVisibleTiles() {
+    return Math.floor((carouselBox.offsetWidth + 10)/(projectTiles[0].offsetWidth + 10))
+}
 
+function setVisibleElements() {
+    let slotsRemaining = nVisibleTiles();
+    for (p of projectTiles) {
+        if (slotsRemaining > 0) {
+            p.style.display ="block";
+            slotsRemaining--
+        } else {
+            p.style.display ="none";
+        }
+    }
+}
+setVisibleElements()
+addEventListener("resize", setVisibleElements)
 // Compute the index based on the listLength and the offset to handle looping
-function indexOffset(index, offset, listLength) {
-    newIndex = index + offset;
-    return newIndex >= 0 ? newIndex%listLength : listLength + newIndex%listLength
-}
-
-// Compute positions of the tiles
-function computeTopLeftCornerPositions(carouselBoxWidth, projectTileWidth, spaceBetweenTiles){
-    let nVisibleTiles = Math.floor((carouselBoxWidth + spaceBetweenTiles)/(projectTileWidth + spaceBetweenTiles))
-    let xTopLeftCorner = {};
-    let computedMargin = carouselBoxWidth/2 - (nVisibleTiles*projectTileWidth - (nVisibleTiles - 1)*spaceBetweenTiles)/2;
-    for (let i = 0; i < nVisibleTiles; i++) {
-        xTopLeftCorner[i] = computedMargin + (spaceBetweenTiles + projectTileWidth)*i
-    }
-    return xTopLeftCorner
-}
-
-// Move tiles to layout of project tiles on page resize assuming all tiles are same size
-// Tiles have an origin set to top left corner
-function layoutProjectTiles(carouselBox, projectTiles, spaceBetweenTiles) {
-    // Compute array of x values
-    let xTopLeftCorner = computeTopLeftCornerPositions(carouselBox.offsetWidth, projectTiles[0].width, spaceBetweenTiles)
-    if (Object.keys(xTopLeftCorner).length == 0) {
-        gsap.set(projectTiles, {scale: carouselBox.offsetWidth/projectTiles[0].offsetWidth, top: 0, left:0})
-    }
-
-}
-
-layoutProjectTiles(carouselBox, projectTiles, 10)
-
-
+// function indexOffset(index, offset, listLength) {
+//     newIndex = index + offset;
+//     return newIndex >= 0 ? newIndex%listLength : listLength + newIndex%listLength
+// }
+//
+// // Compute positions of the tiles
+// function computeTopLeftCornerPositions(carouselBoxWidth, projectTileWidth, spaceBetweenTiles){
+//     let nVisibleTiles = Math.floor((carouselBoxWidth + spaceBetweenTiles)/(projectTileWidth + spaceBetweenTiles))
+//     console.log(nVisibleTiles)
+//     let xTopLeftCorner = {};
+//     let computedMargin = carouselBoxWidth/2 - (nVisibleTiles*projectTileWidth - (nVisibleTiles - 1)*spaceBetweenTiles)/2;
+//     for (let i = 0; i < nVisibleTiles; i++) {
+//         xTopLeftCorner[i] = {x: computedMargin + (spaceBetweenTiles + projectTileWidth)*i}
+//     }
+//     return xTopLeftCorner
+// }
+//
+// // Move tiles to layout of project tiles on page resize assuming all tiles are same size
+// // Tiles have an origin set to top left corner
+// function layoutProjectTiles(carouselBox, projectTiles, spaceBetweenTiles) {
+//     // Compute array of x values
+//     let xTopLeftCornerPositions = computeTopLeftCornerPositions(carouselBox.offsetWidth, projectTiles[0].offsetWidth, spaceBetweenTiles)
+//     console.log(xTopLeftCornerPositions)
+//     let nVisibleTiles = Object.keys(xTopLeftCornerPositions).length
+//     if (nVisibleTiles == 0) {
+//         console.log("scaling")
+//         gsap.set(projectTiles, {scale: carouselBox.offsetWidth/projectTiles[0].offsetWidth, top: 0, left:0})
+//     } else {
+//         for (let i = 0; i < nVisibleTiles; i++) {
+//             gsap.set(projectTiles[i], xTopLeftCornerPositions[i])
+//         }
+//     }
+//
+// }
+// window.addEventListener("resize", (event) => layoutProjectTiles(carouselBox, projectTiles, 10))
+//
+// function slideNext() {
+//     moveAllProjectTiles();
+// }
+//
+// setInterval(increment, 10000, 0)
+//
+// function increment() {
+//      mainIndex++
+// }
+//
+//
+//
+// function setNewAnimations() {
+//
+// }
 // layoutProjectTiles(projectTiles, carouselBox)
 // addEventListener("resize", (event) => {layoutProjectTiles(projectTiles, carouselBox)})
 //
